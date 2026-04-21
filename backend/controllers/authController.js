27bf72933e5f0e25ef47281819e5b8dc1035cbd9
Saif <Saif@dev.dev>
2026-04-21 23:00:05 +0200

    Logout Route & Controller Added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -48,3 +48,12 @@ exports.login = async (req, res) => {
         return res.status(500).json({ success: false, message: "Something went wrong" });
     }   
 }
+
+exports.logout = (req, res) => {
+    try {
+        res.clearCookie("token").status(200).json({ success: true, message: "Logged out successfully" });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ success: false, message: "Something went wrong" });
+    }
+}
--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,9 +1,10 @@
-const { register, login } = require("../controllers/authController");
+const { register, login, logout } = require("../controllers/authController");
 const loginValidator = require("../middlewares/LoginValidator");
 const registerValidator = require("../middlewares/RegisterValidator");
 const router = require("express").Router();
 
 router.post("/register", registerValidator, register);
 router.post("/login", loginValidator, login);
+router.post("/logout", logout);
 
 module.exports = router;
