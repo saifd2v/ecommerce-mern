b98e6f2f928eef0b87a80a6a4b48b7ad949744c6
Saif <Saif@dev.dev>
2026-04-21 20:53:39 +0200

    Main App.js

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,21 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
-require("dotenv");
-
+const cors = require("cors");
+const rateLimiter = require("express-rate-limit");
+require("dotenv").config({ quiet: true });
 
 const app = express();
+const PORT = process.env.PORT;
+const limiter = rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 60,
+    handler: (req, res) => {
+        return res.status(429).json({ message: "Too many requests, try again later" });
+    }
+});
+
+app.use(express.json());
+app.use(cors({ origin: "http://localhost:3000", credentials: true }));
+app.use(cookieParser());
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
