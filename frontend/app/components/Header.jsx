57c5ca8180cef4524202c9d8a756dcb3cd1741cc
Saif <Saif@dev.dev>
2026-04-23 00:53:25 +0200

    Frontend Added

---
+++ frontend/app/components/Header.jsx
@@ -0,0 +1,15 @@
+import Link from 'next/link'
+import React from 'react'
+import { ShoppingBasket } from 'lucide-react'
+
+const Header = () => {
+    return (
+        <header className='bg-white h-32 w-full'>
+            <nav>
+
+            </nav>
+        </header>
+    )
+}
+
+export default Header
---
+++ frontend/app/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from './components/Header'
+
+const page = () => {
+  return (
+    <main className='font-inter w-full min-h-[100vh] bg-blue-700/10'>
+      <Header />
+    </main>
+  )
+}
+
+export default page
